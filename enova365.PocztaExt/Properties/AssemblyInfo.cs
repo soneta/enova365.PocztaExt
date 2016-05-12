1803a2acf71401e7a0e13943b1d6436b7caa1948
tomasz.schabowski <tomasz.schabowski@PREZENTACJA>
2016-05-12 18:10:48 +0200

    Add project files.

---
+++ enova365.PocztaExt/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using enova365.PocztaExt.Utils;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("enova365.PocztaExt")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("enova365.PocztaExt")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d0f41ad2-47ed-483c-8dba-a87521dbbd0d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(Tools.Version + ".*")]
---
+++ enova365.PocztaExt/Rejestracje.cs
@@ -0,0 +1,7 @@
+﻿using Soneta.Business.UI;
+
+[assembly: FolderView("enova365.PocztaExt",
+    Priority = 1000,
+    Description = "Dodatek dla enova365",
+    BrickColor = FolderViewAttribute.YellowBrick
+)]
