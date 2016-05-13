ddb829589de3847a127d1c2b85235698f139ab8b
Tomasz Schabowski <tomasz.schabowski@enova.pl>
2016-05-13 08:30:45 +0200

    ViewInfo, Params

---
+++ enova365.PocztaExt/WiadomosciExt/DokumentyWiadomosciParams.cs
@@ -0,0 +1,47 @@
+﻿using Soneta.Business;
+using Soneta.Handel;
+using Soneta.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace enova365.PocztaExt.WiadomosciExt
+{
+    public class DokumentyWiadomosciParams : ContextBase
+    {
+        private const string Key = "Soneta.PocztaExt.DokumentyWiadomosci";
+
+        public DokumentyWiadomosciParams(Context cx) : base(cx)
+        {
+            Load();
+        }
+
+        [Context]
+        [Caption("Definicja")]
+        public DefDokHandlowego Definicja
+        {
+            get
+            {
+                if (Context.Contains(typeof(DefDokHandlowego)))
+                    return (DefDokHandlowego)Context[typeof(DefDokHandlowego)];
+                return null;
+            }
+            set
+            {
+                Context[typeof(DefDokHandlowego)] = value;
+                Save();
+            }
+        }
+
+        protected void Load()
+        {
+            SetContext(typeof(DefDokHandlowego), Session.Login.Load(this, Key, "Definicja"));
+        }
+
+        protected void Save()
+        {
+            Session.Login.Save(this, Key, "Definicja");
+        }
+    }
+}
