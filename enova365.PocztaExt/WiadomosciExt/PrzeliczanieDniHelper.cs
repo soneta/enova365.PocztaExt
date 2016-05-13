7731e4e247e055c12930e18bcb8f2c255389dad3
Tomasz Schabowski <tomasz.schabowski@enova.pl>
2016-05-13 08:39:15 +0200

    Serwisy

---
+++ enova365.PocztaExt/WiadomosciExt/PrzeliczanieDniHelper.cs
@@ -0,0 +1,30 @@
+﻿using Soneta.Handel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace enova365.PocztaExt.WiadomosciExt
+{
+    public class PrzeliczanieDniHelper
+    {
+        public static void Przelicz(DokumentHandlowy row)
+        {
+            Soneta.Business.Log l = new Soneta.Business.Log("Zliczanie Dni", true);
+            // Jeżeli dokument nie jest w sesji nie kontynuujemy przeliczania
+            if (!row.IsLive || row.Definicja.Symbol != "FV") return;
+            DokumentHandlowy fv = (DokumentHandlowy)row;
+            DokumentHandlowy zo = fv.Nadrzędne[TypRelacjiHandlowej.Kopiowania];
+
+            if (zo != null)
+            {
+                foreach (WiadomoscExt w in PocztaExtModule.GetInstance(row).WiadomosciExt.WgDokument[zo])
+                {
+                    w.CzasRealizacji = (new Soneta.Types.FromTo(w.Wiadomosc.Data, DateTime.Today).Days);
+                }
+
+            }
+
+        }
+    }
+}
---
+++ enova365.PocztaExt/WiadomosciExt/ZatwierdzanieFVService.cs
@@ -0,0 +1,46 @@
+﻿using enova365.PocztaExt.WiadomosciExt;
+using Soneta.Business;
+using Soneta.Handel;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+[assembly: Service(typeof(IZmianaDokumentuHandlowego), typeof(ZatwierdzanieFVService))]
+
+namespace enova365.PocztaExt.WiadomosciExt
+{
+    public class ZatwierdzanieFVService : IZmianaDokumentuHandlowego
+    {
+        public void WyliczenieCenyPozycji(WyliczenieCenyPozycjiDokumentuArgs args)
+        {
+        }
+
+        public void Zatwierdzanie(ZmianaDokumentuHandlowegoArgs args)
+        {
+            Trace.WriteLine("Zatwierdzanie " + args.Dokument + ", edycja: " + args.Edycja, "Zmiana dokumentu");
+        }
+
+        public void Zatwierdzony(ZmianaDokumentuHandlowegoArgs args)
+        {
+            PrzeliczanieDniHelper.Przelicz(args.Dokument);
+        }
+
+        public void ZmianaPozycji(ZmianaPozycjiDokumentuArgs args)
+        {
+        }
+
+        public void ZmianaPłatności(ZmianaDokumentuHandlowegoArgs args)
+        {
+        }
+
+        public void ZmianaStanu(ZmianaStanuDokumentuHandlowegoArgs args)
+        {
+        }
+
+        public void ZmianaWartości(ZmianaDokumentuHandlowegoArgs args)
+        {
+        }
+    }
+}
